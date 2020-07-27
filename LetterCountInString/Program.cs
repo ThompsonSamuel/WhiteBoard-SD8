d3547182977d481913506e97def5fe98f17f000e
ThompsonSamuel <Thompson.Samuel.M@outlook.com>
2020-07-27 10:39:12 -0700

    Added Palindrome

---
+++ LetterCountInString/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace LetterCountInString
+{
+    class Program
+    {
+		//3 - 'H'
+		//Name of Movie
+		//return how many of a letter are in the name
+		public static int LetterCountInString(string title, char searchChar)
+		{
+			int letterCount = 0;
+
+			searchChar = char.ToLower(searchChar);
+			title = title.ToLower();
+			
+				for (int i = 0; i <= title.Length; i++)
+			{
+				if (title[0] == searchChar)
+				{
+					letterCount++;
+				}
+			}
+
+			return letterCount;
+		}
+		static void Main(string[] args)
+        {
+            Console.WriteLine("Program.LetterCountInString()");
+
+			string movieTitle = "Shawshank Redemption";
+			char searchLetter = 'S';
+			int count = LetterCountInString(movieTitle, searchLetter);
+
+            Console.WriteLine($"There are {count} {searchLetter} in {movieTitle}");		
+		}
+    }
+}
---
+++ NewString/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace NewString
+{
+    class Program
+    {
+        static string ReverseString( string inputSring)
+        {
+            string outputString = "";
+            for (int i = inputSring.Length; i > 0;)
+            {
+                outputString += inputSring[--i];
+            }
+            return outputString;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ReverseString.Program.Main()");
+            string myString = "testString";
+            Console.WriteLine($"{myString} <> {ReverseString(myString)}");
+        }
+    }
+}
