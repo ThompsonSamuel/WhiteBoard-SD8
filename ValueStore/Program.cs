c57eb8eedbf842ed2670a668e91940581107c8c5
ThompsonSamuel <Thompson.Samuel.M@outlook.com>
2020-09-24 16:05:15 -0700

    altered keyvaluestore

--- ValueStore/Program.cs
+++ ValueStore/Program.cs
@@ -1,64 +1,79 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Dynamic;
 using System.Linq;
-using System.Security.Cryptography.X509Certificates;
 
 namespace KeyValueStore
 {
-    public struct KeyValue
-    {
-        public readonly string key;
-        public readonly object value;
-
-        public KeyValue(string _key, object _value)
-        {
-            key = _key;
-            value = _value;
-        }
-    }
-    class MyDictionary
-    {
-        Dictionary<string, KeyValue> keyvalues = new Dictionary<string, KeyValue>();
-
-        public object this[string inputKey]
-        {
-            set
-            {
-                if (keyvalues.ContainsKey(inputKey))
-                {
-                    keyvalues[inputKey] = new KeyValue(inputKey, value);
-                }
-                else
-                    keyvalues.Add(inputKey, new KeyValue(inputKey, value));
-            }
-            get
-            {
-                if (keyvalues.ContainsKey(inputKey))
-                {
-                    return keyvalues[inputKey].value;
-                }
-                else
-                    return new KeyNotFoundException();
-            }
-        }
-    }
-
     public class Program
     {
         static void Main()
         {
-            var d = new MyDictionary();
-            try
-            {
-                Console.WriteLine(d["Cats"]);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex);
-            }
-            d["Cats"] = 42;
-            d["Dogs"] = 17;
-            Console.WriteLine($"{d["Cats"]}, {d["Dogs"]}");
+            var list = new List<string>() { "first", "second", "third", "fourth", "fifth" };
+            Console.WriteLine("List<string>");
+            foreach (string i in list) { Console.Write($" - {i}"); }
+
+            var linkedlist = new LinkedList<int>(new[] { 0, 1, 2, 3, 4 });
+            Console.WriteLine("\n\nLinkedList<int>");
+            foreach (int i in linkedlist) { Console.Write($" - {i}"); }
+
+            var queue = new Queue<string>(new[] { "zero", "one", "two", "three", "four" });
+            Console.WriteLine("\n\nQueue<string>");
+            foreach (string i in queue) { Console.Write($" - {i}"); }
+
+            var stack = new Stack<byte>(new byte[] { 0, 1, 2, 3, 4 });
+            Console.WriteLine("\n\nStack<byte>");
+            foreach (byte i in stack) { Console.Write($" - {i}"); }
+
+            var dictionary = new Dictionary<int, bool> { [1] = true, [2] = false, [3] = false, [0] = true, [15] = false };
+            Console.WriteLine("\n\nDictionary<int, bool>");
+            foreach (bool i in dictionary.Values) { Console.Write($" - {i}"); } 
+
+            var sortedlist = new SortedList<string, object> { ["first"] = 1, ["second"] = "filler", ["third"] = 8, ["fourth"] = 0.0005, ["fifth"] = "space" };
+            Console.WriteLine("\n\nSortedList<string, object>");
+            foreach (var i in sortedlist.Values) { Console.Write($" - {i}"); }
+
+            var hashset = new HashSet<string>() { "1", "2", "3", "4", "5" };
+            Console.WriteLine("\n\nHashSet<string>");
+            foreach (string i in hashset) { Console.Write($" - {i}"); }
         }
     }
+    public struct KeyValue<TKey, TValue> where TKey : IComparable
+    {
+        public readonly TKey key;
+        public readonly TValue value;
+
+        public KeyValue(TKey key, TValue value)
+        {
+            this.key = key;
+            this.value = value;
+        }
+    }
+    class MyDictionary<TKey, TValue> where TKey : IComparable
+    {
+        List<KeyValue<TKey, TValue>> keyValues = new List<KeyValue<TKey, TValue>>();
+        public object this[TKey inputKey]
+        {
+            get
+            {
+                foreach(KeyValue<TKey, TValue> i in keyValues)
+                {
+                    if (i.key.Equals(inputKey))
+                        return i.value;
+                }
+                return new KeyNotFoundException();
+            }
+            set
+            {
+                for (int i = 0; i < keyValues.Count; i++)
+                {
+                    if (keyValues[i].key.Equals(inputKey))
+                        keyValues[i] = new KeyValue<TKey, TValue>(inputKey, (TValue)Convert.ChangeType(value, typeof(TValue)));
+                }
+                keyValues.Add(new KeyValue<TKey, TValue>(inputKey, (TValue)Convert.ChangeType(value, typeof(TValue))));
+            }
+        }
+    }
+
 }
